d09eba5c4cb7b9df46ed5449fee72b2b1650504e
pskpretty <prettykurian@Prettys-MacBook-Air.local>
2017-09-14 08:35:13 +1000

    after redirection

--- src/main/java/com/qut/routeOptimizerWebApp/MainClass.java
+++ src/main/java/com/qut/routeOptimizerWebApp/MainClass.java
@@ -1,54 +1,45 @@
 package com.qut.routeOptimizerWebApp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.graphhopper.GHRequest;
+import com.graphhopper.GHResponse;
+import com.graphhopper.GraphHopper;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.util.Instruction;
+import com.graphhopper.util.shapes.GHPoint;
+
+@Controller
 
 public class MainClass {
-/*public String getReponse(NavigationPosition source,NavigationPosition destination,String vehicle) {
-	 String s = "";
-	 GraphHopper graphHopper = new GraphHopper().setGraphHopperLocation(RouteOptimzerProperties.hopperDirectory)
-				.setEncodingManager(new EncodingManager("car")).setOSMFile(RouteOptimzerProperties.osmFilePath);
-	 graphHopper.importOrLoad();
-	 GHRequest request = new GHRequest().addPoint(new GHPoint(source., orig[1]))
-				.addPoint(new GHPoint(dest[0], dest[1])).addPoint(new GHPoint(-27.544866, 153.092784));
-		request.putHint("calcPoints", false);
-		request.putHint("instructions", true);
-		// request.setWeighting("fastest");
-		request.setVehicle("car"); // "car"
-		
-	return s;
-}
-	public static void main(String[] args) throws IOException {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+	    return "index";
+	}
+	
+	@RequestMapping(value = "distance",method = RequestMethod.GET)
+	public String getDistance(@RequestParam(value="sourceArray[]") String[] sourceArray, @RequestParam(value="destinationArray[]") String[] destinationArray) {
+		String s = "";
 		GraphHopper graphHopper = new GraphHopper().setGraphHopperLocation(RouteOptimzerProperties.hopperDirectory)
 				.setEncodingManager(new EncodingManager("car")).setOSMFile(RouteOptimzerProperties.osmFilePath);
-
 		graphHopper.importOrLoad();
-		double[] orig = new double[] { -27.477356, 153.028423 };
-		double[] dest = new double[] { -27.575710, 153.092426 };
-		GHRequest request = new GHRequest().addPoint(new GHPoint(orig[0], orig[1]))
-				.addPoint(new GHPoint(dest[0], dest[1])).addPoint(new GHPoint(-27.544866, 153.092784));
+		GHRequest request = new GHRequest().addPoint(new GHPoint(Double.parseDouble(sourceArray[0]), Double.parseDouble(sourceArray[1])))
+				.addPoint(new GHPoint(Double.parseDouble(destinationArray[0]), Double.parseDouble(destinationArray[1])));
 		request.putHint("calcPoints", false);
 		request.putHint("instructions", true);
-		// request.setWeighting("fastest");
-		request.setVehicle("car"); // "car"
-		
+		request.setVehicle("car"); 
 		GHResponse ghResponse = graphHopper.route(request);
-		 String s = "";
-		 if (ghResponse.getInstructions() != null) {
-		        for (Instruction i : ghResponse.getInstructions()) {
-		            s += "------>\ntime <long>: " + i.getTime() + "\n" + "name: street name" + i.getName() + "\n" +
-		                    "annotation <InstructionAnnotation>" +
-		                    i.getAnnotation() + "\n" + "distance" + i.getDistance() + "\n" + "sign <int>:" + i.getSign() +
-		                    "\n" + "Points <PointsList>: " + i.getPoints() + "\n";
-		        }
-		    }
-		//PointList pl=res.getPoints();
-		//double distance = res.getDistance();
-		// time of the full path, in milliseconds
-		//long millis = res.getMillis();
-		// get information per turn instruction
-		//InstructionList il = res.getInstructions();
-		//System.out.println(request.toString());
-		System.out.println( "\ninstructions\n" + s);
-	}*/
+		if (ghResponse.getInstructions() != null) {
+			for (Instruction i : ghResponse.getInstructions()) {
+				s += "------>\ntime <long>: " + i.getTime() + "\n" + "name: street name" + i.getName() + "\n"
+						+ "annotation <InstructionAnnotation>" + i.getAnnotation() + "\n" + "distance" + i.getDistance()
+						+ "\n" + "sign <int>:" + i.getSign() + "\n" + "Points <PointsList>: " + i.getPoints() + "\n";
+			}
+		}
+		return s;
+	}
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,8 +1,58 @@
 <html>
-<body>
-<h1>Maven + Spring MVC Web Project Example</h1>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
 
-<h2>Message : ${message}</h2>
-<h2>Counter : ${counter}</h2>
+$(document).ready(function () {
+
+    $('#search').click(function () {
+        jQuery.support.cors = true;
+         var source = [];
+         source[0] = Number($('#sourcelat').val()).toFixed(2);
+         source[1] = Number($('#sourcelon').val()).toFixed(2);
+	var destiation=[];
+	destiation[0] = Number($('#destinationlat').val()).toFixed(2);
+	destiation[1] = Number($('#destinationlon').val()).toFixed(2);
+        $.ajax(
+            {
+                type: "GET",
+                url:'http://localhost:8080/routing/distance',
+                        data: {"sourceArray":JSON.stringify(source),"destinationArray":JSON.stringify(destiation)
+                            },
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+			console.log("success"+response);
+                    alert('success'+response);
+                
+                },
+                error: function (msg, url, line) {
+                    alert('error trapped in error: function(msg, url, line)');
+                    alert('msg = ' + msg + ', url = ' + url + ', line = ' + line);
+
+                }
+            });
+
+
+        alert('button click');
+
+    });
+});
+
+</script>  
+</head>
+<body>
+
+		<form
+        id = "form"
+        action = "#"
+        name = "form">
+			Source Latitude<input id="sourcelat" type="text" /><br/>
+			Source Longitude<input id="sourcelon" type="text" /><br/>
+			 Destination Latitude<input id="destinationlat" type="text" />  <br/>
+			 Destination Longitude<input id="destinationlon" type="text" /><br/>
+			<button id="search">Find me a distance</button><br/>
+		</form>
+	
 </body>
+
 </html>
